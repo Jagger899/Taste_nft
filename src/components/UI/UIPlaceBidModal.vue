800be1555d080cb3c74e23a3bc1937ee3eb767b1
Пользователь <romanskarb2@ukr.net>
2025-01-22 19:36:17 +0200

    main: artwork coundown, user data, part mod

---
+++ src/components/UI/UIPlaceBidModal.vue
@@ -0,0 +1,16 @@
+<script setup>
+
+</script>
+
+<template>
+  <div class="place-modal">
+    <div class="place-modal__content">
+      <h2 class="place-modal__title">Place a bid</h2>
+      <div class="place-modal__close" @click="close"></div>
+    </div>
+  </div>
+</template>
+
+<style scoped lang="scss">
+
+</style>
--- src/pages/Artwork.vue
+++ src/pages/Artwork.vue
@@ -38,13 +38,13 @@ const currentNft = ref(nft.find(item => item.id === nftId));
 
             <div class="activity__current-box">
 
-              <BaseSvg id="tongue"/>
+              <BaseSvg id="tongue" class="activity__current-svg"/>
 
               <div class="activity__current-numbers">
 
-                <span class="activity__current-number">1,5M</span>
+                <span class="activity__current-number">{{ currentNft.price.quantity }}M</span>
 
-                <span class="activity__current-number-desc">(1308.54$)</span>
+                <span class="activity__current-number-desc">{{currentNft.price.value}}</span>
 
               </div>
 
@@ -52,11 +52,13 @@ const currentNft = ref(nft.find(item => item.id === nftId));
 
           </div>
           <div class="activity__ending">
-            <p class="activity__title">Current Bid:</p>
+            <p class="activity__title">Auction ending in:</p>
             <CountDown
-              v-if="nft.price.time"
-              :initial-time="nft.price.time * 3600"
+              class="activity__ending-time"
+              v-if="currentNft?.price?.time"
+              :initialTime="currentNft.price.time * 3600"
             />
+            <p v-else class="activity__ending-time">Sold</p>
           </div>
           <div class="activity__button">
 
@@ -70,3 +72,92 @@ const currentNft = ref(nft.find(item => item.id === nftId));
 
   </section>
 </template>
+
+<style lang="scss" scoped>
+@import '@/assets/scss/style';
+
+.artwork {
+  margin-top: 96px;
+
+  @include media-breakpoint-down(xs) {
+    padding-top: unset;
+    height: 400px;
+  }
+
+  &__top-activity {
+    margin: 0 auto;
+  }
+}
+
+.activity {
+  width: 518px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  border-radius: 16px;
+  box-shadow: 0 25px 40px 0 rgba(0, 0, 0, 0.05);
+  background: #30363d;
+  padding: 16px 20px;
+
+  &__current {
+    position: relative;
+    &::after {
+      content: "";
+      position: absolute;
+      background: #ffffff26;
+      width: 3px;
+      height: 43px;
+      border-radius: 12px;
+      right: -20px;
+      bottom: 20px;
+    }
+  }
+
+  &__current-svg {
+    width: 19px;
+    height: 19px;
+  }
+
+  &__current-box {
+    display: flex;
+    align-items: center;
+    gap: 5px;
+  }
+
+  &__current-numbers {
+    display: flex;
+    gap: 5px;
+  }
+
+  &__current-number {
+    font-weight: 600;
+    font-size: 16px;
+    color: $whiteColor;
+  }
+
+  &__current-number-desc {
+    font-weight: 600;
+    font-size: 16px;
+    color: rgba(255, 255, 255, 0.5);
+  }
+
+  &__title {
+    font-weight: 600;
+    font-size: 14px;
+    color: rgba(255, 255, 255, 0.5);
+    margin-bottom: 8px;
+  }
+
+  &__ending-time {
+    font-weight: 600;
+    font-size: 16px;
+    color: #fff;
+    text-align: center;
+  }
+
+  &__button {
+    width: 138px;
+    height: 40px;
+  }
+}
+</style>
